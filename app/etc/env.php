254f0a446a3dbb92fe017fc1c48db1f730464542
Stanisław Kowalski <s.kowalski@gloo.pl>
2019-04-02 09:26:13 +0200

    update

---
+++ app/etc/env.php
@@ -0,0 +1,64 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => 'd96e9b2e8fba0dbcc1f8f0c2772f4382'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'dev8.pro',
+                'dbname' => 'admin_radler',
+                'username' => 'admin_radler',
+                'password' => 'DZ33UUIj5',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '672_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '672_'
+            ]
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'install' => [
+        'date' => 'Wed, 27 Mar 2019 11:07:18 +0000'
+    ]
+];
