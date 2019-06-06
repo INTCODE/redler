9d002bbc0ebf0d3c420bd6a5900f2e8227e61947
Wojciech Chudek <w.chudek@gloo.pl>
2019-06-06 10:51:11 +0200

    cron - action - approve

--- app/code/Blm/CronTab/Controller/Adminhtml/Items/MassDelete.php
+++ app/code/Blm/CronTab/Controller/Adminhtml/Items/MassDelete.php
@@ -51,10 +51,21 @@ class MassDelete extends \Magento\Backend\App\Action
         $collectionSize = $collection->getSize();
 
         foreach ($collection as $record) {
-            $record->delete();
+            $date=time();
+            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+            $customerObj = $objectManager->create('Magento\Customer\Model\Customer')->load($record['entity_id']);
+            $effectiveDate = date('Y-m-d', strtotime("+3 months", strtotime($customerObj['CheckedDate'])));
+            $customerObj['approve_account']=2;
+            $customerObj['CheckedDate']=$effectiveDate;
+            $customerObj->save();
+
+
+
+           // file_put_contents("testowyxd.txt", file_get_contents("testowyxd.txt")."\n=========adres===========\n".print_r($customerObj->debug(), true));
+            //file_put_contents("testowyxd.txt", file_get_contents("testowyxd.txt")."\n=========date===========\n".print_r($effectiveDate, true));
         }
 
-        $this->messageManager->addSuccess(__('A total of %1 record(s) have been deleted.', $collectionSize));
+        $this->messageManager->addSuccess(__('A total of %1 record(s) have been approved.', $collectionSize));
 
         /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
         $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
