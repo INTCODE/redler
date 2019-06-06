b7f91894964b0af16a832d83f846239c77632b67
Stanisław Kowalski <s.kowalski@gloo.pl>
2019-06-06 13:04:34 +0200

    Documents tab

---
+++ app/code/Blm/CustomerDocuments/Block/Adminhtml/Items.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Blm\CustomerDocuments\Block\Adminhtml;
+
+class Items extends \Magento\Backend\Block\Widget\Grid\Container
+{
+    /**
+     * Constructor
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_controller = 'items';
+        $this->_headerText = __('Items');
+        $this->_addButtonLabel = __('Add New Item');
+        parent::_construct();
+    }
+}
