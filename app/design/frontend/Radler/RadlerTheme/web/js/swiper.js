1bab0cf80e0c157ab2c9a85abcca457659f3b99c
aleksanderpiron1 <pironaleksander@gmail.com>
2019-06-11 00:35:33 +0200

    mini cart restyle and some other fixes

--- app/design/frontend/Radler/RadlerTheme/web/js/swiper.js
+++ app/design/frontend/Radler/RadlerTheme/web/js/swiper.js
@@ -35,11 +35,27 @@ require([
 });
 
 require([
-    'jquery',
-    'slick'
+    'jquery'
 ], function ($) {
     $('.collapse-btn').click(function(){
         var target = $(this).closest('.question');
         target.toggleClass('collapsed')
     })
 });
+// HIDE HEADER ON SCROLL
+// require([
+//     'jquery'
+// ], function ($) {
+//     var lastScrollTop = 0;
+//     $(window).scroll(function(event){
+//         if($(window).width()<991){
+//             var st = $(this).scrollTop();
+//             if (st > lastScrollTop && st > 160 && !$('.minicart-wrapper').hasClass('active')){
+//                $('.page-header').css('top', '-800px');
+//             } else {
+//                $('.page-header').css('top', '0');
+//             }
+//             lastScrollTop = st;
+//         }
+//     })
+// });
