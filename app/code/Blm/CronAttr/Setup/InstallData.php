b5ce16dcd005902beca29ede007592382e5336cd
Stanisław Kowalski <s.kowalski@gloo.pl>
2019-07-03 15:48:57 +0200

    attribute fix

---
+++ app/code/Blm/CronAttr/Setup/InstallData.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Blm\CronAttr\Setup;
+
+use Magento\Customer\Model\Customer;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Eav\Model\Entity\Attribute\Set as AttributeSet;
+use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * @var CustomerSetupFactory
+     */
+    protected $customerSetupFactory;
+
+    /**
+     * @var AttributeSetFactory
+     */
+    private $attributeSetFactory;
+
+    /**
+     * @param CustomerSetupFactory $customerSetupFactory
+     * @param AttributeSetFactory $attributeSetFactory
+     */
+    public function __construct(
+        EavSetupFactory $eavSetupFactory,
+        CustomerSetupFactory $customerSetupFactory,
+        AttributeSetFactory $attributeSetFactory
+    ) {
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->attributeSetFactory = $attributeSetFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        /**
+         *  Customer attributes
+         */
+        /** @var CustomerSetup $customerSetup */
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
+
+        $customerEntity = $customerSetup->getEavConfig()->getEntityType('customer');
+        $attributeSetId = $customerEntity->getDefaultAttributeSetId();
+
+        /** @var $attributeSet AttributeSet */
+        $attributeSet = $this->attributeSetFactory->create();
+        $attributeGroupId = $attributeSet->getDefaultGroupId($attributeSetId);       
+
+        $customerSetup->addAttribute(Customer::ENTITY, 'CheckedDate',
+            [
+                'type' => 'datetime',
+                'label' => 'Aproved to',
+                'input' => 'datetime',
+                'source' => '',
+                'required' => false,
+                'default' => '0',
+                'visible' => true,
+                'user_defined' => false,
+                'sort_order' => 220,
+                'position' => 220,
+                'system' => false,
+                'is_used_in_grid' => true,
+                'is_visible_in_grid' => true,
+                'is_filterable_in_grid' => true,
+                'is_searchable_in_grid' => true,
+            ]);
+        $CheckedDate = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'CheckedDate')
+            ->addData([
+                'attribute_set_id' => $attributeSetId,
+                'attribute_group_id' => $attributeGroupId,
+                'used_in_forms' => ['adminhtml_customer', 'customer_account_create', 'customer_account_edit'],
+            ]);
+        $CheckedDate->save();
+
+        $setup->endSetup();
+    }
+
+}
